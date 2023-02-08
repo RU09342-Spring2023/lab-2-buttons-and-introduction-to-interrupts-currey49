87b5d228de0d9d3023f204c26871a0c687b2d5f1
currey49 <123091601+currey49@users.noreply.github.com>
2023-02-08 18:10:54 -0500

    Update Part1.c

--- 1 Buttons and Pullup PullDown Resistors/Part1.c
+++ 1 Buttons and Pullup PullDown Resistors/Part1.c
@@ -1,8 +1,8 @@
 /*
  *  Pull-Up Resistor Configuration
  *
- *  Created on: Jan 31, 2023
- *      Author: Russell
+ *  Created on: Feb 2, 2023
+ *  *      Author: Matthew Currey
  *      Version: 1.0
  *
  *      This example will show you how to configure the Pull-up Resistor for your button inputs.
@@ -28,15 +28,22 @@ int main(void)
 
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mode
                                             // to activate previously configured port settings
-
+P1OUT &= ~BIT0;
+P6OUT &= ~BIT6;
 
     while(1)
     {
-        if (P2IN & BIT3)            // If S2 (P2.3) is pressed
+
+        if (!(P2IN & BIT3))            // If S2 (P2.3) is pressed
+        {
             P6OUT ^= BIT6;          // Toggle P6.6
-        if (P4IN & BIT1)            // If S1 (P4.1) is pressed
+        }
+        if (!(P4IN & BIT1))            // If S1 (P4.1) is pressed
+        {
             P1OUT ^= BIT0;          // Toggle P1.0
+        }
         __delay_cycles(100000);             // Delay for 100000*(1/MCLK)=0.1s
+
     }
 
 
